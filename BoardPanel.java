272215c93ee6f9b1275d7a9d232bcfb4aa16af96
Mano <36997897+EmmanuelOladipo@users.noreply.github.com>
2019-03-20 12:48:08 +0000

    Add files via upload

--- BoardPanel.java
+++ BoardPanel.java
@@ -130,12 +130,12 @@ class BoardPanel {
     private void createRedCheckers() {
         int numReds = 0;
         int currentPip = 13; // PIP TO BE CONVERTED TO COLUMN
-        int column = convertPipToColumn(currentPip); // COLUMN OF PIP THAT WAS CONVERTED
+        int column = GameLogic.convertPipToColumn(currentPip); // COLUMN OF PIP THAT WAS CONVERTED
         int row;
 
         for(int numCheckersInPip = 0; numCheckersInPip < 5; numCheckersInPip++) // Counts the checkers
         {
-            row = nextRow(column, currentPip, 0);
+            row = GameLogic.nextRow(column, currentPip, 0);
             Circle red = new Circle(BoardPanel.BOARD[column][row].getX(), BoardPanel.BOARD[column][row].getY(), RADIUS); // Circle with coordinates
             BoardPanel.BOARD[column][row].occupyCoordinate(); // Position now occupied
             red.setStroke(Color.BLACK);
@@ -145,11 +145,11 @@ class BoardPanel {
         }
 
         currentPip = 24;
-        column = convertPipToColumn(currentPip);
+        column = GameLogic.convertPipToColumn(currentPip);
 
         for(int numCheckersInPip = 0; numCheckersInPip < 2; numCheckersInPip++)
         {
-            row = nextRow(column, currentPip, 0);
+            row = GameLogic.nextRow(column, currentPip, 0);
             Circle red = new Circle(BoardPanel.BOARD[column][row].getX(), BoardPanel.BOARD[column][row].getY(), RADIUS);
             BoardPanel.BOARD[column][row].occupyCoordinate();
             red.setStroke(Color.BLACK);
@@ -160,11 +160,11 @@ class BoardPanel {
         }
 
         currentPip = 8;
-        column = convertPipToColumn(currentPip);
+        column = GameLogic.convertPipToColumn(currentPip);
 
         for(int numCheckersInPip = 0; numCheckersInPip < 3; numCheckersInPip++)
         {
-            row = nextRow(column, currentPip, 0);
+            row = GameLogic.nextRow(column, currentPip, 0);
             Circle red = new Circle(BoardPanel.BOARD[column][row].getX(), BoardPanel.BOARD[column][row].getY(), RADIUS);
             BoardPanel.BOARD[column][row].occupyCoordinate();
             red.setStroke(Color.BLACK);
@@ -174,11 +174,11 @@ class BoardPanel {
         }
 
         currentPip = 6;
-        column = convertPipToColumn(currentPip);
+        column = GameLogic.convertPipToColumn(currentPip);
 
         for(int numCheckersInPip = 0; numCheckersInPip < 5; numCheckersInPip++)
         {
-            row = nextRow(column, currentPip, 0);
+            row = GameLogic.nextRow(column, currentPip, 0);
             Circle red = new Circle(BoardPanel.BOARD[column][row].getX(), BoardPanel.BOARD[column][row].getY(), RADIUS);
             BoardPanel.BOARD[column][row].occupyCoordinate();
             red.setStroke(Color.BLACK);
@@ -191,12 +191,12 @@ class BoardPanel {
     private void createBlueCheckers() {
         int numBlues = 0;
         int currentPip = 17; // Pip to be placed on
-        int column = convertPipToColumn(currentPip);
+        int column = GameLogic.convertPipToColumn(currentPip);
         int row;
 
         for(int numCheckersInPip = 0; numCheckersInPip < 3; numCheckersInPip++)
         {
-            row = nextRow(column, currentPip, 0);
+            row = GameLogic.nextRow(column, currentPip, 0);
             Circle blue = new Circle(BoardPanel.BOARD[column][row].getX(), BoardPanel.BOARD[column][row].getY(), RADIUS);
             BoardPanel.BOARD[column][row].occupyCoordinate();
             blue.setStroke(Color.BLACK);
@@ -206,11 +206,11 @@ class BoardPanel {
         }
 
         currentPip = 19;
-        column = convertPipToColumn(currentPip);
+        column = GameLogic.convertPipToColumn(currentPip);
 
         for(int numCheckersInPip = 0; numCheckersInPip < 5; numCheckersInPip++)
         {
-            row = nextRow(column, currentPip, 0);
+            row = GameLogic.nextRow(column, currentPip, 0);
             Circle blue = new Circle(BoardPanel.BOARD[column][row].getX(), BoardPanel.BOARD[column][row].getY(), RADIUS);
             BoardPanel.BOARD[column][row].occupyCoordinate();
             blue.setStroke(Color.BLACK);
@@ -220,11 +220,11 @@ class BoardPanel {
         }
 
         currentPip = 12;
-        column = convertPipToColumn(currentPip);
+        column = GameLogic.convertPipToColumn(currentPip);
 
         for(int numCheckersInPip = 0; numCheckersInPip < 5; numCheckersInPip++)
         {
-            row = nextRow(column, currentPip,0);
+            row = GameLogic.nextRow(column, currentPip,0);
             Circle blue = new Circle(BoardPanel.BOARD[column][row].getX(), BoardPanel.BOARD[column][row].getY(), RADIUS);
             BoardPanel.BOARD[column][row].occupyCoordinate();
             blue.setStroke(Color.BLACK);
@@ -233,11 +233,11 @@ class BoardPanel {
             numBlues++;        }
 
         currentPip = 1;
-        column = convertPipToColumn(currentPip);
+        column = GameLogic.convertPipToColumn(currentPip);
 
         for(int numCheckersInPip = 0; numCheckersInPip < 2; numCheckersInPip++)
         {
-            row = nextRow(column, currentPip, 0);
+            row = GameLogic.nextRow(column, currentPip, 0);
             Circle blue = new Circle(BoardPanel.BOARD[column][row].getX(), BoardPanel.BOARD[column][row].getY(), RADIUS);
             BoardPanel.BOARD[column][row].occupyCoordinate();
             blue.setStroke(Color.BLACK);
@@ -246,358 +246,6 @@ class BoardPanel {
             numBlues++;        }
     }
 
-    // CAN BE REMOVED AFTER SPRINT 3
-    static void setUpCheat() {
-        for(int i = 0; i < 12; i++) {
-            for(int j = 0; j < 12; j++) {
-                BoardPanel.BOARD[i][j].releaseCoordinate();
-            }
-        }
-
-        for(int i = 0; i < 1; i++) {
-            for(int j = 0; j < 30; j++) {
-                BoardPanel.BEAR[i][j].releaseCoordinate();
-            }
-        }
-
-        for(int i = 0; i < 1; i++) {
-            for(int j = 0; j < 10; j++) {
-                BoardPanel.BAR[i][j].releaseCoordinate();
-            }
-        }
-
-        int numReds = 0;
-        int numBlues = 0;
-        int currentPip = 1; // PIP TO BE CONVERTED TO COLUMN
-        int column = convertPipToColumn(currentPip); // COLUMN OF PIP THAT WAS CONVERTED
-        int row;
-
-        for(int numCheckersInPip = 0; numCheckersInPip < 2; numCheckersInPip++) // Counts the checkers
-        {
-            row = nextRow(column, currentPip, 0);
-            Checkers.moveCircle(redCheckers[numReds].getCircle(), column, row, currentPip);
-            BoardPanel.BOARD[column][row].occupyCoordinate(); // Position now occupied
-            numReds++;
-        }
-
-        currentPip = 2;
-        column = convertPipToColumn(currentPip);
-        for(int numCheckersInPip = 0; numCheckersInPip < 2; numCheckersInPip++) // Counts the checkers
-        {
-            row = nextRow(column, currentPip, 0);
-            Checkers.moveCircle(redCheckers[numReds].getCircle(), column, row, currentPip);
-            BoardPanel.BOARD[column][row].occupyCoordinate(); // Position now occupied
-            numReds++;
-        }
-
-        currentPip = 3;
-        column = convertPipToColumn(currentPip);
-        for(int numCheckersInPip = 0; numCheckersInPip < 2; numCheckersInPip++) // Counts the checkers
-        {
-            row = nextRow(column, currentPip, 0);
-            Checkers.moveCircle(redCheckers[numReds].getCircle(), column, row, currentPip);
-            BoardPanel.BOARD[column][row].occupyCoordinate(); // Position now occupied
-            numReds++;
-        }
-
-        currentPip = 4;
-        column = convertPipToColumn(currentPip);
-        for(int numCheckersInPip = 0; numCheckersInPip < 2; numCheckersInPip++) // Counts the checkers
-        {
-            row = nextRow(column, currentPip, 0);
-            Checkers.moveCircle(redCheckers[numReds].getCircle(), column, row, currentPip);
-            BoardPanel.BOARD[column][row].occupyCoordinate(); // Position now occupied
-            numReds++;
-        }
-
-        currentPip = 5;
-        column = convertPipToColumn(currentPip);
-        for(int numCheckersInPip = 0; numCheckersInPip < 2; numCheckersInPip++) // Counts the checkers
-        {
-            row = nextRow(column, currentPip, 0);
-            Checkers.moveCircle(redCheckers[numReds].getCircle(), column, row, currentPip);
-            BoardPanel.BOARD[column][row].occupyCoordinate(); // Position now occupied
-            numReds++;
-        }
-
-        column = 0; // BAR RED
-        row = 0;
-        for(int numCheckersInPip = 0; numCheckersInPip < 3; numCheckersInPip++) {
-            Checkers.moveCircle(redCheckers[numReds].getCircle(), column, row, 0);
-            BoardPanel.BAR[column][row].occupyCoordinate();
-            numReds++;
-            row++;
-        }
-
-        column = 0; // BEAR RED
-        row = 15;
-        for(int numCheckersInPip = 0; numCheckersInPip < 2; numCheckersInPip++) {
-            Checkers.moveCircle(redCheckers[numReds].getCircle(), column, row, 25);
-            BoardPanel.BEAR[column][row].occupyCoordinate();
-            numReds++;
-            row++;
-        }
-
-        column = 0; // BAR BLUE
-        row = 5;
-        for(int numCheckersInPip = 0; numCheckersInPip < 3; numCheckersInPip++) {
-            Checkers.moveCircle(blueCheckers[numBlues].getCircle(), column, row, 0);
-            BoardPanel.BAR[column][row].occupyCoordinate();
-            numBlues++;
-            row++;
-        }
-
-        column = 0; // BEAR BLUE
-        row = 0;
-        for(int numCheckersInPip = 0; numCheckersInPip < 3; numCheckersInPip++) {
-            Checkers.moveCircle(blueCheckers[numBlues].getCircle(), column, row, 25);
-            BoardPanel.BEAR[column][row].occupyCoordinate();
-            numBlues++;
-            row++;
-        }
-
-
-        currentPip = 24;
-        column = convertPipToColumn(currentPip);
-        for(int numCheckersInPip = 0; numCheckersInPip < 3; numCheckersInPip++) // Counts the checkers
-        {
-            row = nextRow(column, currentPip, 0);
-            Checkers.moveCircle(blueCheckers[numBlues].getCircle(), column, row, currentPip);
-            BoardPanel.BOARD[column][row].occupyCoordinate(); // Position now occupied
-            numBlues++;
-        }
-
-        currentPip = 22;
-        column = convertPipToColumn(currentPip);
-        for(int numCheckersInPip = 0; numCheckersInPip < 3; numCheckersInPip++) // Counts the checkers
-        {
-            row = nextRow(column, currentPip, 0);
-            Checkers.moveCircle(blueCheckers[numBlues].getCircle(), column, row, currentPip);
-            BoardPanel.BOARD[column][row].occupyCoordinate(); // Position now occupied
-            numBlues++;
-        }
-
-        currentPip = 21;
-        column = convertPipToColumn(currentPip);
-        for(int numCheckersInPip = 0; numCheckersInPip < 3; numCheckersInPip++) // Counts the checkers
-        {
-            row = nextRow(column, currentPip, 0);
-            Checkers.moveCircle(blueCheckers[numBlues].getCircle(), column, row, currentPip);
-            BoardPanel.BOARD[column][row].occupyCoordinate(); // Position now occupied
-            numBlues++;
-        }
-    }
-
-    static int getWinner() { // Check if last position in Bear Off is taken
-        if(BoardPanel.BEAR[0][29].isTaken()) { // If last position for reds Bear off is taken then red wins
-            // PLAYER RED WINS
-            return Player.playerRed.getTurn();
-        }
-
-        if(BoardPanel.BEAR[0][14].isTaken()) { // If last position for blues Bear off is taken then blue wins
-            // PLAYER BLUE WINS
-            return Player.playerBlue.getTurn();
-        }
-        return 0;
-    }
-
-    // Convert pip to a column integer which is used as X index in array
-    static int convertPipToColumn(int pip) {
-        int column = 0;
-
-        if(pip>12) { // TOP HALF OF BOARD
-            column = pip-13;
-        }
-
-        if(pip <= 12) { // BOTTOM HALF OF BOARD
-            column = Math.abs(pip-12);
-        }
-
-        if(pip == 0 || pip == 25) { // BAR OR BEAR
-            column = 0;
-        }
-
-        return column;
-    }
-
-    // Look for available row in pip to place checker, checks who's turn it is
-    static int nextRow(int column, int pip, int currentPlayer) {
-        int freeRow = -1;
-
-        if(currentPlayer == 0 || currentPlayer == Player.playerRed.getTurn()) { // IF BOARD IS NOT FLIPPED
-            if(pip > 12) { // Top half of board
-                for(int rowCheck = 0; rowCheck < 6; rowCheck++) { // Max 6 checkers in each column // Start at top and go down
-                    if(!BoardPanel.BOARD[column][rowCheck].isTaken()) { // If position in column is free then break
-                        freeRow = rowCheck;
-                        break;
-                    }
-
-                    if(rowCheck == 5) { // Pip is full with 6 checkers
-                        break;
-                    }
-                }
-            }
-
-            if(pip <= 12) { // Bottom half of board
-                for(int rowCheck = 11; rowCheck > 5; rowCheck--) { // Max 6 checkers in each column // Start bottom and go up
-                    if(!BoardPanel.BOARD[column][rowCheck].isTaken()) { // If position in column is free then break
-                        freeRow = rowCheck;
-                        break;
-                    }
-
-                    if(rowCheck == 6) { // Pip is full with 6 checkers
-                        break;
-                    }
-                }
-            }
-
-            if(pip == 0) {
-                for(int rowCheck = 0; rowCheck < 10; rowCheck++) {
-                    if(!BoardPanel.BAR[0][rowCheck].isTaken()) {
-                        freeRow = rowCheck;
-                        break;
-                    }
-                }
-            }
-
-            if(pip == 25) {
-                for(int rowCheck = 15; rowCheck < 30; rowCheck++) {
-                    if(!BoardPanel.BEAR[0][rowCheck].isTaken()) {
-                        freeRow = rowCheck;
-                        break;
-                    }
-                }
-            }
-        }
-
-        else if(currentPlayer == Player.playerBlue.getTurn()) { // IF BOARD IS FLIPPED
-            if(pip > 12) { // Bottom half of the board
-                for(int rowCheck = 11; rowCheck > 5; rowCheck--) { // Max 6 checkers in each column // Start bottom and go up
-                    if(!BoardPanel.BOARD[column][rowCheck].isTaken()) { // If position in column is free then break
-                        freeRow = rowCheck;
-                        break;
-                    }
-
-                    if(rowCheck == 6) { // Pip is full with 6 checkers
-                        break;
-                    }
-                }
-            }
-
-            if(pip <= 12) { // Top half of the board
-                for(int rowCheck = 0; rowCheck < 6; rowCheck++) { // Max 6 checkers in each column // Start at top and go down
-                    if(!BoardPanel.BOARD[column][rowCheck].isTaken()) { // If position in column is free then break
-                        freeRow = rowCheck;
-                        break;
-                    }
-
-                    if(rowCheck == 5) { // Pip is full with 6 checkers
-                        break;
-                    }
-                }
-            }
-
-            if(pip == 0) {
-                for(int rowCheck = 0; rowCheck < 10; rowCheck++) {
-                    if(!BoardPanel.BAR[0][rowCheck].isTaken()) {
-                        freeRow = rowCheck;
-                        break;
-                    }
-                }
-            }
-
-            if(pip == 25) {
-                for(int rowCheck = 0; rowCheck < 15; rowCheck++) {
-                    if(!BoardPanel.BEAR[0][rowCheck].isTaken()) {
-                        freeRow = rowCheck;
-                        break;
-                    }
-                }
-            }
-        }
-        return freeRow; // Return position in column that is free
-    }
-
-    static int topCheckerInPip(int column, int pip, int currentPlayer) {
-        int top = 0;
-
-        if(currentPlayer == Player.playerRed.getTurn()) {
-            if(pip >= 13) { // Top half of board
-                for(int nextRow = 0; nextRow <= 6; nextRow++) { // Max 6 checkers in each column // Start at top and go down
-                    if(!BoardPanel.BOARD[column][nextRow].isTaken()) { // If position in column is free then break
-                        top = nextRow - 1; // Decrement by 1 to get top checker, as counting down from the top
-                        break;
-                    }
-                    if(nextRow == 5) { // If 6 checkers in a column,
-                        top = 5;
-                        break;
-                    }
-                }
-            }
-
-            if(pip <= 12) { // Bottom half of board
-                for(int nextRow = 11; nextRow >= 6; nextRow--) { // Max 6 checkers in each column // Start bottom and go up
-                    if(!BoardPanel.BOARD[column][nextRow].isTaken()) { // If position in column is free then break and return checker before it
-                        top = nextRow + 1; // Increment by 1 to get top checker, as counting up from the bottom
-                        break;
-                    }
-                    if(nextRow == 6) { // If 6 checkers in a column, return top checker
-                        top = 6;
-                        break;
-                    }
-                }
-            }
-
-            if(pip == 0) {
-                for(int nextRow = 0; nextRow < 5; nextRow++) {
-                    if(!BoardPanel.BAR[0][nextRow].isTaken()) {
-                        top = nextRow - 1;
-                        break;
-                    }
-                }
-            }
-        }
-
-        else if(currentPlayer == Player.playerBlue.getTurn()) {
-            if(pip >= 13) { // Bottom half of board
-                for(int nextRow = 11; nextRow >= 6; nextRow--) { // Max 6 checkers in each column // Start bottom and go up
-                    if(!BoardPanel.BOARD[column][nextRow].isTaken()) { // If position in column is free then break and return checker before it
-                        top = nextRow + 1; // Increment by 1 to get top checker, as counting up from the bottom
-                        break;
-                    }
-                    if(nextRow == 6) { // If 6 checkers in a column, return top checker
-                        top = 6;
-                        break;
-                    }
-                }
-            }
-
-            if(pip <= 12) { // Top half of board
-                for(int nextRow = 0; nextRow <= 6; nextRow++) { // Max 6 checkers in each column // Start at top and go down
-                    if(!BoardPanel.BOARD[column][nextRow].isTaken()) { // If position in column is free then break
-                        top = nextRow - 1; // Decrement by 1 to get top checker, as counting down from the top
-                        break;
-                    }
-                    if(nextRow == 5) { // If 6 checkers in a column,
-                        top = 5;
-                        break;
-                    }
-                }
-            }
-
-            if(pip == 0) {
-                for(int nextRow = 5; nextRow < 10; nextRow++) {
-                    if(!BoardPanel.BAR[0][nextRow].isTaken()) {
-                        top = nextRow - 1;
-                        break;
-                    }
-                }
-            }
-        }
-        return top; // Return position in column that is free
-    }
-
-    // CAN BE REMOVED ONCE LEGAL MOVES CAN BE DISPLAYED
     static Circle checkerAtStartingPip(int column, int row, int currentPlayer, int pip) {
         Circle toBeMoved = null;
 
